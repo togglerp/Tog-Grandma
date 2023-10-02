d4889442ec7d718d7f3c7b340a6f8b37b484afda
Toggle <80492091+togglerp@users.noreply.github.com>
2023-10-02 18:13:15 +0100

    Created fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Toggle'
+version '1.1'
+
+client_script 'client/*.lua'
+server_script 'server/*.lua'
+
+shared_scripts {
+	'shared/*.lua',
+}
+
+lua54 'yes'
